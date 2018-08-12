7b8bb2519eef055b951173d9d05d24558ca66139
shamaton <shamaton77@gmail.com>
2018-08-12 23:22:36 +0900

    developing

--- serialize/array.go
+++ serialize/array.go
@@ -1,7 +1,6 @@
 package serialize
 
 import (
-	"encoding/hex"
 	"fmt"
 	"math"
 	"reflect"
@@ -13,7 +12,7 @@ type serializer struct {
 	common
 }
 
-func AsArray(v interface{}) /*([]byte, error)*/ {
+func AsArray(v interface{}) ([]byte, error) {
 	s := serializer{}
 
 	rv := reflect.ValueOf(v)
@@ -24,42 +23,75 @@ func AsArray(v interface{}) /*([]byte, error)*/ {
 		}
 	}
 
-	size := s.calcSize(rv)
+	size, err := s.calcSize(rv)
+	if err != nil {
+		return nil, err
+	}
+
 	s.d = make([]byte, size)
-	s.create(rv, 0)
-	fmt.Println(def.Byte4, size, s)
-	fmt.Println(hex.Dump(s.d))
+	last, err := s.create(rv, 0)
+	if err != nil {
+		return nil, err
+	}
+	if size != last {
+		return nil, fmt.Errorf("failed serialization size=%d, lastIdx=%d", size, last)
+	}
+	return s.d, err
 }
 
-func (s *serializer) calcSize(rv reflect.Value) int {
-	ret := 0
+func (s *serializer) calcSize(rv reflect.Value) (int, error) {
+	ret := def.Byte1
 
 	switch rv.Kind() {
 	case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uint:
 		v := rv.Uint()
-		ret = s.calcUint(v)
+		ret += s.calcUint(v)
 
 	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int:
 		v := rv.Int()
 		if v >= 0 {
-			s.calcUint(uint64(v))
+			ret += s.calcUint(uint64(v))
 		} else if s.isNegativeFixInt64(v) {
-			ret = def.Byte1
+			// format code only
 		} else if v >= math.MinInt8 {
-			ret = def.Byte1 + def.Byte1
+			ret += +def.Byte1
 		} else if v >= math.MinInt16 {
-			ret = def.Byte1 + def.Byte2
+			ret += +def.Byte2
 		} else if v >= math.MinInt32 {
-			ret = def.Byte1 + def.Byte4
+			ret += +def.Byte4
 		} else {
-			ret = def.Byte1 + def.Byte8
+			ret += def.Byte8
+		}
+
+	case reflect.Array, reflect.Slice:
+		l := rv.Len()
+
+		// format size
+		if l <= 0x0f {
+			// format code only
+		} else if l <= math.MaxUint16 {
+			ret += def.Byte2
+		} else if l <= math.MaxUint32 {
+			ret += def.Byte4
+		} else {
+			// not supported error
+			return 0, fmt.Errorf("not support this array length : %d", l)
+		}
+
+		// objects size
+		for i := 0; i < l; i++ {
+			s, err := s.calcSize(rv.Index(i))
+			if err != nil {
+				return 0, err
+			}
+			ret += s
 		}
 	}
 
-	return ret
+	return ret, nil
 }
 
-func (s *serializer) create(rv reflect.Value, offset int) {
+func (s *serializer) create(rv reflect.Value, offset int) (int, error) {
 
 	switch rv.Kind() {
 	case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uint:
@@ -69,7 +101,7 @@ func (s *serializer) create(rv reflect.Value, offset int) {
 	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int:
 		v := rv.Int()
 		if v >= 0 {
-			s.writeUint(uint64(v), offset)
+			offset = s.writeUint(uint64(v), offset)
 		} else if s.isNegativeFixInt64(v) {
 			offset = s.writeSize1Int64(v, offset)
 		} else if v >= math.MinInt8 {
@@ -85,20 +117,48 @@ func (s *serializer) create(rv reflect.Value, offset int) {
 			offset = s.writeSize1Int(def.Int64, offset)
 			offset = s.writeSize8Int64(v, offset)
 		}
+
+	case reflect.Array, reflect.Slice:
+		l := rv.Len()
+
+		// format
+		if l <= 0x0f {
+			offset = s.writeSize1Int(def.FixArray+l, offset)
+		} else if l <= math.MaxUint16 {
+			offset = s.writeSize1Int(def.Array16, offset)
+			offset = s.writeSize2Int(l, offset)
+		} else if l <= math.MaxUint32 {
+			offset = s.writeSize1Int(def.Array32, offset)
+			offset = s.writeSize4Int(l, offset)
+		} else {
+			// not supported error
+			return 0, fmt.Errorf("not support this array length : %d", l)
+		}
+
+		// objects
+		for i := 0; i < l; i++ {
+			var err error
+			offset, err = s.create(rv.Index(i), offset)
+			if err != nil {
+				return 0, err
+			}
+		}
 	}
+	return offset, nil
 }
 
 func (s *serializer) calcUint(v uint64) int {
-	size := def.Byte1
+	size := 0
 	if v <= math.MaxInt8 {
+		// format code only
 	} else if v <= math.MaxUint8 {
-		size += def.Byte1
+		size = def.Byte1
 	} else if v <= math.MaxUint16 {
-		size += def.Byte2
+		size = def.Byte2
 	} else if v <= math.MaxUint32 {
-		size += def.Byte4
+		size = def.Byte4
 	} else {
-		size += def.Byte8
+		size = def.Byte8
 	}
 	return size
 }
