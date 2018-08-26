e09568d9aa0654ce2012fe68f11c135d93c3a910
shamaton <shamaton77@gmail.com>
2018-08-26 22:42:05 +0900

    developing

--- deserialize/int.go
+++ deserialize/int.go
@@ -7,52 +7,52 @@ import (
 	"github.com/shamaton/msgpack/def"
 )
 
-func (d *deserializer) asUint(offset int, k reflect.Kind) (uint64, int, error) {
+func (d *deserializer) asInt(offset int, k reflect.Kind) (int64, int, error) {
 
 	code := d.data[offset]
 
 	if d.isPositiveFixNum(code) {
 		b, offset := d.readSize1(offset)
-		return uint64(b), offset, nil
+		return int64(b), offset, nil
 	} else if d.isNegativeFixNum(code) {
 		b, offset := d.readSize1(offset)
-		return uint64(int8(b)), offset, nil
+		return int64(int8(b)), offset, nil
 	} else if code == def.Uint8 {
 		offset++
 		b, offset := d.readSize1(offset)
-		return uint64(uint8(b)), offset, nil
+		return int64(uint8(b)), offset, nil
 	} else if code == def.Int8 {
 		offset++
 		b, offset := d.readSize1(offset)
-		return uint64(int8(b)), offset, nil
+		return int64(int8(b)), offset, nil
 	} else if code == def.Uint16 {
 		offset++
 		bs, offset := d.readSize2(offset)
 		v := binary.BigEndian.Uint16(bs)
-		return uint64(v), offset, nil
+		return int64(v), offset, nil
 	} else if code == def.Int16 {
 		offset++
 		bs, offset := d.readSize2(offset)
 		v := int16(binary.BigEndian.Uint16(bs))
-		return uint64(v), offset, nil
+		return int64(v), offset, nil
 	} else if code == def.Uint32 {
 		offset++
 		bs, offset := d.readSize4(offset)
 		v := binary.BigEndian.Uint32(bs)
-		return uint64(v), offset, nil
+		return int64(v), offset, nil
 	} else if code == def.Int32 {
 		offset++
 		bs, offset := d.readSize4(offset)
 		v := int32(binary.BigEndian.Uint32(bs))
-		return uint64(v), offset, nil
+		return int64(v), offset, nil
 	} else if code == def.Uint64 {
 		offset++
 		bs, offset := d.readSize8(offset)
-		return binary.BigEndian.Uint64(bs), offset, nil
+		return int64(binary.BigEndian.Uint64(bs)), offset, nil
 	} else if code == def.Int64 {
 		offset++
 		bs, offset := d.readSize8(offset)
-		return binary.BigEndian.Uint64(bs), offset, nil
+		return int64(binary.BigEndian.Uint64(bs)), offset, nil
 	} else if code == def.Nil {
 		offset++
 		return 0, offset, nil
