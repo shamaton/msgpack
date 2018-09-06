fcd5ada611469a92fc49c754f13addf989bd35a6
shamaton <shamaton77@gmail.com>
2018-09-06 23:48:55 +0900

    add comments to public methods

--- decode.go
+++ decode.go
@@ -2,10 +2,14 @@ package msgpack
 
 import "github.com/shamaton/msgpack/internal/decoding"
 
+// DecodeStructAsMap decodes data that is encoded as map format.
+// This is the same thing that StructAsArray sets false.
 func DecodeStructAsMap(data []byte, v interface{}) error {
 	return decoding.Decode(data, v, false)
 }
 
+// DecodeStructAsArray decodes data that is encoded as array format.
+// This is the same thing that StructAsArray sets true.
 func DecodeStructAsArray(data []byte, v interface{}) error {
 	return decoding.Decode(data, v, true)
 }
