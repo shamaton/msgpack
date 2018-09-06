456b69304e0895fbb1eb48bd1a41c252912cc8a9
shamaton <shamaton77@gmail.com>
2018-09-06 23:48:55 +0900

    add comments to public methods

--- encode.go
+++ encode.go
@@ -4,10 +4,14 @@ import (
 	"github.com/shamaton/msgpack/internal/encoding"
 )
 
+// EncodeStructAsMap encodes data as map format.
+// This is the same thing that StructAsArray sets false.
 func EncodeStructAsMap(v interface{}) ([]byte, error) {
 	return encoding.Encode(v, false)
 }
 
+// EncodeStructAsArray encodes data as array format.
+// This is the same thing that StructAsArray sets true.
 func EncodeStructAsArray(v interface{}) ([]byte, error) {
 	return encoding.Encode(v, true)
 }
