3a55394d9ed90a991694a761f4b2b86b75df9544
shamaton <shamaton77@gmail.com>
2018-09-17 23:30:54 +0900

    done todo some tasks

--- internal/decoding/int.go
+++ internal/decoding/int.go
@@ -8,71 +8,74 @@ import (
 )
 
 func (d *decoder) isPositiveFixNum(v byte) bool {
-	// todo : refactor
-	if def.PositiveFixIntMin <= v && v <= def.PositiveFixIntMax {
-		return true
-	}
-	return false
+	return def.PositiveFixIntMin <= v && v <= def.PositiveFixIntMax
 }
 
 func (d *decoder) isNegativeFixNum(v byte) bool {
-	// todo : refactor
-	if def.NegativeFixintMin <= int8(v) && int8(v) <= def.NegativeFixintMax {
-		return true
-	}
-	return false
+	return def.NegativeFixintMin <= int8(v) && int8(v) <= def.NegativeFixintMax
 }
 
 func (d *decoder) asInt(offset int, k reflect.Kind) (int64, int, error) {
 
 	code := d.data[offset]
 
-	// todo : use switch
-	if d.isPositiveFixNum(code) {
+	switch {
+	case d.isPositiveFixNum(code):
 		b, offset := d.readSize1(offset)
 		return int64(b), offset, nil
-	} else if d.isNegativeFixNum(code) {
+
+	case d.isNegativeFixNum(code):
 		b, offset := d.readSize1(offset)
 		return int64(int8(b)), offset, nil
-	} else if code == def.Uint8 {
+
+	case code == def.Uint8:
 		offset++
 		b, offset := d.readSize1(offset)
 		return int64(uint8(b)), offset, nil
-	} else if code == def.Int8 {
+
+	case code == def.Int8:
 		offset++
 		b, offset := d.readSize1(offset)
 		return int64(int8(b)), offset, nil
-	} else if code == def.Uint16 {
+
+	case code == def.Uint16:
 		offset++
 		bs, offset := d.readSize2(offset)
 		v := binary.BigEndian.Uint16(bs)
 		return int64(v), offset, nil
-	} else if code == def.Int16 {
+
+	case code == def.Int16:
 		offset++
 		bs, offset := d.readSize2(offset)
 		v := int16(binary.BigEndian.Uint16(bs))
 		return int64(v), offset, nil
-	} else if code == def.Uint32 {
+
+	case code == def.Uint32:
 		offset++
 		bs, offset := d.readSize4(offset)
 		v := binary.BigEndian.Uint32(bs)
 		return int64(v), offset, nil
-	} else if code == def.Int32 {
+
+	case code == def.Int32:
 		offset++
 		bs, offset := d.readSize4(offset)
 		v := int32(binary.BigEndian.Uint32(bs))
 		return int64(v), offset, nil
-	} else if code == def.Uint64 {
+
+	case code == def.Uint64:
 		offset++
 		bs, offset := d.readSize8(offset)
 		return int64(binary.BigEndian.Uint64(bs)), offset, nil
-	} else if code == def.Int64 {
+
+	case code == def.Int64:
 		offset++
 		bs, offset := d.readSize8(offset)
 		return int64(binary.BigEndian.Uint64(bs)), offset, nil
-	} else if code == def.Nil {
+
+	case code == def.Nil:
 		offset++
 		return 0, offset, nil
 	}
+
 	return 0, 0, d.errorTemplate(code, k)
 }
