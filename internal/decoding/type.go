b4d1ec0f4bb6c49a24a12abfd82725d60da60b1d
shamaton <shamaton77@gmail.com>
2018-09-18 19:19:08 +0900

    done some todo tasks

--- internal/decoding/type.go
+++ internal/decoding/type.go
@@ -1,14 +1,1 @@
 package decoding
-
-import "reflect"
-
-// todo : move to slice or map
-var (
-	typeIntSlice   = reflect.TypeOf([]int{})
-	typeInt8Slice  = reflect.TypeOf([]int8{})
-	typeInt16Slice = reflect.TypeOf([]int16{})
-	typeInt32Slice = reflect.TypeOf([]int32{})
-	typeInt64Slice = reflect.TypeOf([]int64{})
-
-	typeMapStringInt = reflect.TypeOf(map[string]int{})
-)
