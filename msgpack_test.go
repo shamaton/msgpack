44b4143d58cb8fd0ef4aecf5bade69b534f08c99
shamaton <shamaton77@gmail.com>
2021-02-18 13:07:01 +0900

    fix lint

--- msgpack_test.go
+++ msgpack_test.go
@@ -1899,7 +1899,7 @@ func TestExt(t *testing.T) {
 			Uint32:      math.MaxUint32,
 			Uint64:      math.MaxUint32 + 1,
 			Byte2Int:    rand.Intn(math.MaxUint16) - math.MinInt16,
-			Byte4Int:    rand.Intn(math.MaxInt32) - rand.Intn(math.MaxInt32),
+			Byte4Int:    rand.Intn(math.MaxInt32) - rand.Intn(math.MaxInt32-1),
 			Byte4Uint32: math.MaxUint32 - 1,
 			Bytes:       []byte{1, 2, 3}, // 3
 		}
@@ -1934,7 +1934,7 @@ func TestExt(t *testing.T) {
 			Uint32:      math.MaxUint32,
 			Uint64:      math.MaxUint32 + 1,
 			Byte2Int:    rand.Intn(math.MaxUint16) - math.MinInt16,
-			Byte4Int:    rand.Intn(math.MaxInt32) - rand.Intn(math.MaxInt32),
+			Byte4Int:    rand.Intn(math.MaxInt32) - rand.Intn(math.MaxInt32-1),
 			Byte4Uint32: math.MaxUint32 - 1,
 			Bytes:       []byte{4, 5, 6}, // 3
 		}
