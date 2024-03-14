4bda28d420e6f90c380c6454b6965cce8a4a4218
shamaton <shamaton77@gmail.com>
2024-03-14 22:14:00 +0900

    imp

--- ext/encode_stream.go
+++ ext/encode_stream.go
@@ -18,9 +18,6 @@ func (c *StreamEncoderCommon) WriteByte1Int64(w io.Writer, value int64, buf *com
 	return buf.Write(w,
 		byte(value),
 	)
-	//buf.B1[0] = byte(value)
-	//_, err := w.Write(buf.B1)
-	//return err
 }
 
 func (c *StreamEncoderCommon) WriteByte2Int64(w io.Writer, value int64, buf *common.Buffer) error {
@@ -28,10 +25,6 @@ func (c *StreamEncoderCommon) WriteByte2Int64(w io.Writer, value int64, buf *com
 		byte(value>>8),
 		byte(value),
 	)
-	//buf.B2[0] = byte(value >> 8)
-	//buf.B2[1] = byte(value)
-	//_, err := w.Write(buf.B2)
-	//return err
 }
 
 func (c *StreamEncoderCommon) WriteByte4Int64(w io.Writer, value int64, buf *common.Buffer) error {
@@ -41,12 +34,6 @@ func (c *StreamEncoderCommon) WriteByte4Int64(w io.Writer, value int64, buf *com
 		byte(value>>8),
 		byte(value),
 	)
-	//buf.B4[0] = byte(value >> 24)
-	//buf.B4[1] = byte(value >> 16)
-	//buf.B4[2] = byte(value >> 8)
-	//buf.B4[3] = byte(value)
-	//_, err := w.Write(buf.B4)
-	//return err
 }
 
 func (c *StreamEncoderCommon) WriteByte8Int64(w io.Writer, value int64, buf *common.Buffer) error {
@@ -60,25 +47,12 @@ func (c *StreamEncoderCommon) WriteByte8Int64(w io.Writer, value int64, buf *com
 		byte(value>>8),
 		byte(value),
 	)
-	//buf.B8[0] = byte(value >> 56)
-	//buf.B8[1] = byte(value >> 48)
-	//buf.B8[2] = byte(value >> 40)
-	//buf.B8[3] = byte(value >> 32)
-	//buf.B8[4] = byte(value >> 24)
-	//buf.B8[5] = byte(value >> 16)
-	//buf.B8[6] = byte(value >> 8)
-	//buf.B8[7] = byte(value)
-	//_, err := w.Write(buf.B8)
-	//return err
 }
 
 func (c *StreamEncoderCommon) WriteByte1Uint64(w io.Writer, value uint64, buf *common.Buffer) error {
 	return buf.Write(w,
 		byte(value),
 	)
-	//buf.B1[0] = byte(value)
-	//_, err := w.Write(buf.B1)
-	//return err
 }
 
 func (c *StreamEncoderCommon) WriteByte2Uint64(w io.Writer, value uint64, buf *common.Buffer) error {
@@ -86,10 +60,6 @@ func (c *StreamEncoderCommon) WriteByte2Uint64(w io.Writer, value uint64, buf *c
 		byte(value>>8),
 		byte(value),
 	)
-	//buf.B2[0] = byte(value >> 8)
-	//buf.B2[1] = byte(value)
-	//_, err := w.Write(buf.B2)
-	//return err
 }
 
 func (c *StreamEncoderCommon) WriteByte4Uint64(w io.Writer, value uint64, buf *common.Buffer) error {
@@ -99,12 +69,6 @@ func (c *StreamEncoderCommon) WriteByte4Uint64(w io.Writer, value uint64, buf *c
 		byte(value>>8),
 		byte(value),
 	)
-	//buf.B4[0] = byte(value >> 24)
-	//buf.B4[1] = byte(value >> 16)
-	//buf.B4[2] = byte(value >> 8)
-	//buf.B4[3] = byte(value)
-	//_, err := w.Write(buf.B4)
-	//return err
 }
 
 func (c *StreamEncoderCommon) WriteByte8Uint64(w io.Writer, value uint64, buf *common.Buffer) error {
@@ -118,25 +82,12 @@ func (c *StreamEncoderCommon) WriteByte8Uint64(w io.Writer, value uint64, buf *c
 		byte(value>>8),
 		byte(value),
 	)
-	//buf.B8[0] = byte(value >> 56)
-	//buf.B8[1] = byte(value >> 48)
-	//buf.B8[2] = byte(value >> 40)
-	//buf.B8[3] = byte(value >> 32)
-	//buf.B8[4] = byte(value >> 24)
-	//buf.B8[5] = byte(value >> 16)
-	//buf.B8[6] = byte(value >> 8)
-	//buf.B8[7] = byte(value)
-	//_, err := w.Write(buf.B8)
-	//return err
 }
 
 func (c *StreamEncoderCommon) WriteByte1Int(w io.Writer, value int, buf *common.Buffer) error {
 	return buf.Write(w,
 		byte(value),
 	)
-	//buf.B1[0] = byte(value)
-	//_, err := w.Write(buf.B1)
-	//return err
 }
 
 func (c *StreamEncoderCommon) WriteByte2Int(w io.Writer, value int, buf *common.Buffer) error {
@@ -144,10 +95,6 @@ func (c *StreamEncoderCommon) WriteByte2Int(w io.Writer, value int, buf *common.
 		byte(value>>8),
 		byte(value),
 	)
-	//buf.B2[0] = byte(value >> 8)
-	//buf.B2[1] = byte(value)
-	//_, err := w.Write(buf.B2)
-	//return err
 }
 
 func (c *StreamEncoderCommon) WriteByte4Int(w io.Writer, value int, buf *common.Buffer) error {
@@ -157,12 +104,6 @@ func (c *StreamEncoderCommon) WriteByte4Int(w io.Writer, value int, buf *common.
 		byte(value>>8),
 		byte(value),
 	)
-	//buf.B4[0] = byte(value >> 24)
-	//buf.B4[1] = byte(value >> 16)
-	//buf.B4[2] = byte(value >> 8)
-	//buf.B4[3] = byte(value)
-	//_, err := w.Write(buf.B4)
-	//return err
 }
 
 func (c *StreamEncoderCommon) WriteByte4Uint32(w io.Writer, value uint32, buf *common.Buffer) error {
@@ -172,16 +113,8 @@ func (c *StreamEncoderCommon) WriteByte4Uint32(w io.Writer, value uint32, buf *c
 		byte(value>>8),
 		byte(value),
 	)
-	//buf.B4[0] = byte(value >> 24)
-	//buf.B4[1] = byte(value >> 16)
-	//buf.B4[2] = byte(value >> 8)
-	//buf.B4[3] = byte(value)
-	//_, err := w.Write(buf.B4)
-	//return err
 }
 
 func (c *StreamEncoderCommon) WriteBytes(w io.Writer, bs []byte, buf *common.Buffer) error {
 	return buf.Write(w, bs...)
-	//_, err := w.Write(bs)
-	//return err
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/shamaton/msgpack/v2
 
-go 1.18
+go 1.20
