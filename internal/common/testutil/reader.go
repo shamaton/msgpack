8bbffc9dd3e5b59314758c3b53b52ee0bc76a3af
shamaton <shamaton77@gmail.com>
2024-08-03 22:32:00 +0900

    add decoding ext tests

--- internal/common/testutil/reader.go
+++ internal/common/testutil/reader.go
@@ -1,6 +1,9 @@
 package testutil
 
-import "errors"
+import (
+	"errors"
+	"io"
+)
 
 var ErrReaderErr = errors.New("reader error")
 
@@ -12,4 +15,28 @@ func NewErrReader() *ErrReader {
 
 func (ErrReader) Read(_ []byte) (int, error) {
 	return 0, ErrReaderErr
+}
+
+type TestReader struct {
+	s     []byte
+	i     int64 // current reading index
+	count int
+}
+
+func NewTestReader(b []byte) *TestReader {
+	return &TestReader{s: b, i: 0, count: 0}
+}
+
+func (r *TestReader) Read(b []byte) (n int, err error) {
+	if r.i >= int64(len(r.s)) {
+		return 0, io.EOF
+	}
+	n = copy(b, r.s[r.i:])
+	r.i += int64(n)
+	r.count++
+	return
+}
+
+func (r *TestReader) Count() int {
+	return r.count
 }
